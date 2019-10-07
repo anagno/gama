3227c3bb3eac3c6282cffd0974624b967630add0
anagno <anagnwstopoulos@hotmail.com>
2019-10-07 21:03:57 +0200

    Add option for the sqlite3 plugin

--- conanfile.py
+++ conanfile.py
@@ -4,5 +4,12 @@ class Gama(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
    generators = "cmake_find_package"
 
+   options = { "sqlite3" : [True, False],}
+
+   default_options = {"sqlite3": False}
+
    def requirements(self):
        self.requires("Expat/2.2.9@pix4d/stable")
+
+       if self.options.sqlite3:
+           self.requires("sqlite3/3.29.0@bincrafters/stable")
