8ce28079ee60f82d0e7075cadae83dd4c0e65e32
anagno <anagnwstopoulos@hotmail.com>
2019-10-18 23:33:18 +0200

    Add option for the libxml2 and enabling gama-g3 UT

--- conanfile.py
+++ conanfile.py
@@ -4,12 +4,22 @@ class Gama(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
    generators = "cmake_find_package"
 
-   options = { "sqlite3" : [True, False],}
+   options = {
+       "sqlite3": [True, False],
+       "libxml2": [True, False],
+       }
 
-   default_options = {"sqlite3": False}
+   default_options = {
+       "sqlite3": False,
+       "libxml2": False,
+       }
 
    def requirements(self):
        self.requires("Expat/2.2.9@pix4d/stable")
 
        if self.options.sqlite3:
            self.requires("sqlite3/3.29.0@bincrafters/stable")
+
+       if self.options.libxml2:
+           self.requires("libxml2/2.9.9@bincrafters/stable")
+
---
+++ tests/gama-g3/CMake_TestFiles.cmake
@@ -0,0 +1,15 @@
+# Test files
+set(INPUT_FILES
+    "demo-g3-01"
+    "demo-g3-02"
+    "demo-g3-03")
+
+set(test_1_10_files
+    "demo-g3-01-adj.xml"
+    "demo-g3-02-adj.xml"
+    "demo-g3-03-adj.xml")
+
+set(SJTSK05_FILES
+    "dopnul"
+    "vyberova_udrzba")
+
