82db2c76063a834327422879c1b3ab02bd105b63
anagno <anagnwstopoulos@hotmail.com>
2020-08-01 11:56:14 +0200

    Fixing the ci

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake
+from conans import tools
 import platform
 
 # cmake /home/anagno/Documents/projects/gama -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=TRUE -DUSE_SQLITE3=TRUE -DCONAN_EXPORTED=TRUE
@@ -17,30 +18,51 @@ class GaMa(ConanFile):
    version = "2.7"
    settings = "os", "compiler", "build_type", "arch",
    generators = ["cmake_find_package", "virtualrunenv"]
+   exports_sources = {
+       "*",
+       "!.git/*",
+       "!.travis/*",
+       "!.var/*",
+       "!.vscode/*"
+   }
 
    options = {
        "sqlite3": [True, False],
-       "libxml2": [True, False],
        "shared": [True, False],
-       "native_compiler": "ANY",
-       }
+       "native_compiler": "ANY"
+   }
 
    default_options = {
        "sqlite3": False,
-       "libxml2": False,
        "shared": False,
        "native_compiler": None,
-       }
+   }
+
+   #def configure(self):
+      # https://github.com/conan-io/conan-center-index/pull/2274
+      # if self.should_test and not tools.cross_building(self.settings):
+      #   if self.options.libxml2 and self.options["libxml2"].include_utils == False:
+      #      raise ConanInvalidConfiguration(
+      #          "LibXml2 should contain the tools")
 
    def requirements(self):
-       self.requires("expat/2.2.9")
-       self.requires("boost/1.73.0")
+      self.requires("expat/2.2.9")
+      self.requires("boost/1.73.0")
 
-       if self.options.sqlite3:
-           self.requires("sqlite3/3.29.0")
+      if self.options.sqlite3:
+         self.requires("sqlite3/3.29.0")
 
-       if self.options.libxml2:
-           self.requires("libxml2/2.9.9")
+      # This is a build_requiremes but for the moment we do not have a good
+      # way of declaring it 
+      # https://github.com/conan-io/conan/issues/4893
+      # https://github.com/conan-io/conan/issues/7423
+      if self.should_test and not tools.cross_building(self):
+         self.requires("libxml2/2.9.9@#98c13df03fac70408756b07364074446")
+
+   def build_requirements(self):
+
+      if tools.cross_building(self):
+         self.build_requires("expat/2.2.9")
 
    def imports(self):
       self.copy("*.dll", "bin", "bin")
@@ -52,28 +74,37 @@ class GaMa(ConanFile):
          if self.options.native_compiler:
             cmake.definitions["BUILD_TOOS_CMAKE_CXX_COMPILER"] = self.options.native_compiler
          else:
-            raise ConanInvalidConfiguration("A native compiler was not specified")
+            raise ConanInvalidConfiguration(
+                "A native compiler was not specified")
 
       cmake.definitions["CONAN_EXPORTED"] = True
 
       if self.options.sqlite3:
          cmake.definitions["USE_SQLITE3"] = "ON"
 
-      if self.options.libxml2:
-         cmake.definitions["USE_LIBXML2"] = "ON"
+      if self.should_test and not tools.cross_building(self.settings):
          cmake.definitions["BUILD_TESTING"] = "ON"
 
       #cmake.verbose = True
-      cmake.configure()
+      if self.should_configure:
+         cmake.configure()
+
       return cmake
 
    def build(self):
       cmake = self._configure_cmake()
-      cmake.build()
+
+      if self.should_build:
+         cmake.build()
+
+      if self.should_test and not tools.cross_building(self.settings):
+         cmake.test()
 
    def package(self):
       cmake = self._configure_cmake()
-      cmake.install()
+
+      if self.should_install:
+         cmake.install()
 
 
 # sudo apt-get install clang nodejs
